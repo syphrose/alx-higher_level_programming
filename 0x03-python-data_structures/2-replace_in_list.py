615998a4804090e1de7df235360fcde206c221f9
syphrose <swabulwenyi@gmail.com>
2023-06-10 10:16:59 -0700

    Replace an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
+#!/usr/bin/python3
+# 2-replace_in_list.py
 
-my_list = [10, 20, 30, 40, 50]
-print(replace_in_list(my_list, 3, 55))
-print(replace_in_list(my_list, -1, 40))
-print(replace_in_list(my_list, 5, 35))
+def replace_in_list(my_list, idx, element):    
+    """Replace an element of a list at a specific position."""
+    if idx < 0 or idx >= len(my_list):
+        my_list[idx] = element
+    return my_list
