65245fe38b41bb3a5983cf39bffc9875192f78f1
syphrose <swabulwenyi@gmail.com>
2023-06-10 10:30:10 -0700

    Retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+#!/usr/bin/python3
+# 1-element_at.py
 
-my_list = [10, 20, 30, 40, 50]
-print(element_at(my_list, 2))
-print(element_at(my_list, -2))
-print(element_at(my_list, 6))
-print(element_at(my_list, 4))
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
