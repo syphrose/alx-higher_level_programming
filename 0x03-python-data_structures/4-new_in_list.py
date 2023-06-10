62346debe8e12fe2579272c64d70cdf34286cdd9
syphrose <swabulwenyi@gmail.com>
2023-06-10 10:49:22 -0700

    Replace an elemment as a copy without modifying

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    else:
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return new_list
+#!/usr/bin/python3
+# 4-new_in_list.py
 
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
