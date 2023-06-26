7411073640ad0522e74c9905ead1eb32321ba86e
syphrose <swabulwenyi@gmail.com>
2023-06-26 15:26:41 -0700

    Prints x elements of a list

---
+++ 0x05-python-exceptions/0-main.py
@@ -0,0 +1,12 @@
+guillaume@ubuntu:~/0x05$ cat 0-main.py
+#!/usr/bin/python3
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
