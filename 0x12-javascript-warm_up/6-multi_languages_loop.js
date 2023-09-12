b46c0d1e58cf6d7758dc389c4e4451b8194857dd
syphrose <swabulwenyi@gmail.com>
2023-09-12 10:54:17 -0700

    Looping and array of string

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const array = [`C is fun`, `Python is cool`, `JavaScript is amazing`];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let a = 0; a < array.length; a++) {
   console.log(array[a]);
