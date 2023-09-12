77eb1ff7f43b0d0e7a1cdc287164e2bd969be780
syphrose <swabulwenyi@gmail.com>
2023-09-12 11:11:06 -0700

    Printing the n times *C is fun*

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const arg2 = process.argv[2];
-const x = parseInt(arg2);
+const x = Math.floor(Number(process.argv[2]));
 
 if (isNaN(x)) {
   console.log('Missing number of occurrrences');
