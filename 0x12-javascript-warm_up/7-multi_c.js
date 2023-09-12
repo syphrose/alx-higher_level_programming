47566a3c2b9849d73afc81f023cc442c1a6cf5c6
syphrose <swabulwenyi@gmail.com>
2023-09-12 11:07:18 -0700

    Printing the n times *C is fun*

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const arg2 = process.argv[2];
 const x = parseInt(arg2);
 
 if (isNaN(x)) {
-  console.log(`Missing number of occurrrences`);
+  console.log('Missing number of occurrrences');
 } else {
   for (let a = 0; a < x; a++) {
-   console.log(`C is fun`);
+   console.log('C is fun');
   }
 }
