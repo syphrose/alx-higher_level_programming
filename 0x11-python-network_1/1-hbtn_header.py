266732cd868140f3e80268740f916788fb676dea
syphrose <swabulwenyi@gmail.com>
2023-09-29 13:57:22 -0700

    Receiving requests

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Taking  in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
+
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
