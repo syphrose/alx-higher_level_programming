fc77844b5a51327516eaec1dbebf74f46794a218
syphrose <swabulwenyi@gmail.com>
2023-09-29 17:44:56 -0700

    The status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
